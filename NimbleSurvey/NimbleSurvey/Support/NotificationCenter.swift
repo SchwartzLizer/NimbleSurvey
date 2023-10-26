715d049e52cd317a0bf96542d95935475ac293f2
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:50:39 +0700

    Add NotificationCenter.swift to project
    - Add NotificationCenter.swift file to project.pbxproj
    - Define a new notification name "refreshSurvey" in the extension of Notification.Name

---
+++ NimbleSurvey/NimbleSurvey/Support/NotificationCenter.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationCenter.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let refreshSurvey = Notification.Name("refreshSurvey")
+}
