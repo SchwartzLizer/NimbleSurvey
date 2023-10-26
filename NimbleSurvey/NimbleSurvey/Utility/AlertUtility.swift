72f30d6ff19279d4605f29c3a93dce082ba537ec
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:44:32 +0700

    Add AlertUtility.swift to the project
    - Added AlertUtility.swift file to the Utility group
    - Implemented showAlert() function to display an alert with an OK button
    - Implemented showConfirmationAlert() function to display an alert with OK and Cancel buttons
    - Implemented showActionSheet() function to display an action sheet with custom actions

--- NimbleSurvey/NimbleSurvey/Utility/AlertUtility.swift
+++ NimbleSurvey/NimbleSurvey/Utility/AlertUtility.swift
@@ -5,4 +5,110 @@
 //  Created by Tanatip Denduangchai on 10/27/23.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - AlertUtility
+
+class AlertUtility {
+
+    // MARK: - Alert with OK Button
+
+    static func showAlert(title: String?, message: String?, completion: (() -> Void)? = nil) {
+        guard let topVC = UIApplication.topViewController() else { return }
+
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: Constants.Alert.okButtonTitle, style: .default) { _ in
+            completion?()
+        }
+        alertController.addAction(okAction)
+        topVC.present(alertController, animated: true, completion: nil)
+    }
+
+
+    // MARK: - Alert with OK and Cancel Buttons
+
+    static func showConfirmationAlert(
+        title: String?,
+        message: String?,
+        okButtonTitle: String = Constants.Alert.okButtonTitle,
+        cancelButtonTitle: String = Constants.Alert.cancelButtonTitle,
+        okCompletion: (() -> Void)? = nil,
+        cancelCompletion: (() -> Void)? = nil)
+    {
+        guard let topVC = UIApplication.topViewController() else { return }
+
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: okButtonTitle, style: .default) { _ in
+            okCompletion?()
+        }
+        let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
+            cancelCompletion?()
+        }
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        topVC.present(alertController, animated: true, completion: nil)
+    }
+
+
+    // MARK: - Action Sheet
+
+    static func showActionSheet(title: String?, message: String?, actions: [UIAlertAction]) {
+        guard let topVC = UIApplication.topViewController() else { return }
+
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        for action in actions {
+            alertController.addAction(action)
+        }
+        // This is to handle iPads, otherwise it will crash because action sheets on iPads need a source view or bar button item.
+        if let popoverController = alertController.popoverPresentationController, let sourceView = topVC.view {
+            popoverController.sourceView = sourceView
+            popoverController.sourceRect = CGRect(x: sourceView.bounds.midX, y: sourceView.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        topVC.present(alertController, animated: true, completion: nil)
+    }
+
+}
+
+// MARK: - Constants.Alert
+
+extension Constants {
+    enum Alert {
+        static let okButtonTitle = "OK"
+        static let cancelButtonTitle = "Cancel"
+    }
+}
+
+extension UIApplication {
+
+    // MARK: Internal
+
+    class func topViewController(base: UIViewController? = keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return self.topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return self.topViewController(base: selected)
+        }
+        if let presented = base?.presentedViewController {
+            return self.topViewController(base: presented)
+        }
+        return base
+    }
+
+    // MARK: Private
+
+    private class var keyWindow: UIWindow? {
+        // For iOS 13 and later
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .filter { $0.activationState == .foregroundActive }
+                .compactMap { $0 as? UIWindowScene }
+                .first?.windows
+                .first(where: { $0.isKeyWindow })
+        } else {
+            // For iOS 12 and earlier
+            return UIApplication.shared.windows.first(where: { $0.isKeyWindow })
+        }
+    }
+}
---
+++ NimbleSurvey/NimbleSurvey/Utility/CustomPageControl.swift
@@ -0,0 +1,8 @@
+//
+//  CustomPageControl.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
