c865a7a29a5b4696d8c5e48c430ac722be88cdea
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:28:24 +0700

    Update LoginViewController.swift and Login.storyboard in the NimbleSurvey project.

--- NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewController.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  NimbleSurvey
 //
 //  Created by Tanatip Denduangchai on 10/27/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
