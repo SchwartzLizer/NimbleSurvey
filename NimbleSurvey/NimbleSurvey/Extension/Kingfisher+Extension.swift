03412bad86091d1751ef3389be57ebd2e47d3892
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:48:39 +0700

    Add Kingfisher+Extension.swift for image loading and caching
    This commit adds the file Kingfisher+Extension.swift, which contains extensions to UIImageView for loading and caching images using the Kingfisher library. The extensions provide methods to set an image from a URL with optional placeholder and to set a circular profile image from a URL with optional placeholder. The images are scaled, cached, and faded in during transition.

---
+++ NimbleSurvey/NimbleSurvey/Extension/Kingfisher+Extension.swift
@@ -0,0 +1,51 @@
+//
+//  Kingfisher+Extension.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Kingfisher
+import UIKit
+
+extension UIImageView {
+    func setImageFromURL(url: URL?, placeholder: UIImage? = nil) {
+        let processor = DownsamplingImageProcessor(size: self.bounds.size)
+        self.contentMode = .scaleAspectFill
+        self.kf.setImage(
+            with: url,
+            placeholder: placeholder,
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage,
+                .processor(processor),
+                .transition(.fade(0.3)),
+            ]) { result in
+                switch result {
+                case .success: break
+                case .failure: break
+                }
+            }
+    }
+
+    func setProfileFromURL(url: URL?, placeholder: UIImage? = nil) {
+        let processor = DownsamplingImageProcessor(size: self.bounds.size) |>
+            RoundCornerImageProcessor(cornerRadius: self.bounds.size.height / 2)
+        self.contentMode = .scaleAspectFill
+        self.kf.setImage(
+            with: url,
+            placeholder: placeholder,
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage,
+                .processor(processor),
+                .transition(.fade(0.3)),
+            ]) { result in
+                switch result {
+                case .success: break
+                case .failure: break
+                }
+            }
+    }
+}
+
