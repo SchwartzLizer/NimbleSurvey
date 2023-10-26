3e4361c75912521218f477e3966450f779b931b6
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:15:13 +0700

    Add Logger utility for printing debug messages
    - Added Logger.swift file in the Utility folder
    - Implemented a public enum Logger with a static print function that prints debug messages using debugPrint()

---
+++ NimbleSurvey/NimbleSurvey/Utility/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+public enum Logger {
+    public static func print(_ string: String) {
+        debugPrint(string)
+    }
+}
