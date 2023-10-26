90f282d3361efd53bdacc333c707d9054fffe79c
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:43:35 +0700

    Add support for Constants.swift and Localizable.xcstrings
    - Added Constants.swift file to the project
    - Added Localizable.xcstrings file to the project, containing localized strings for various keys

---
+++ NimbleSurvey/NimbleSurvey/Support/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+enum Constants {
+    enum Keys {
+        static let loginBTN = "LoginBTN_Key"
+        static let forgotBTN = "ForgotBTN_Key"
+        static let resetBTN = "ResetBTN_Key"
+        static let emailTF = "emailTF_Key"
+        static let passwordTF = "passwordTF_Key"
+        static let todaySurvey = "TodaySurvey_Key"
+        static let logoutBTN = "LogoutBTN_Key"
+        static let refresherTokenError = "RefresherTokenError_Key"
+        static let resetPasswordDescription = "ResetPasswordDescription_Key"
+        static let errorNoRefreshTokenFound = "Error_NoRefreshTokenFound"
+        static let errorNoAccessTokenFound = "Error_NoAccessTokenFound"
+        static let notificationResetEmailTitle = "Notification_ResetEmailTitle"
+        static let notificationResetEmailMessage = "Notification_ResetEmailMessage"
+    }
+
+    enum ServiceKeys {
+        static let key = "ofzl-2h5ympKa0WqqTzqlVJUiRsxmXQmt5tkgrlWnOE"
+        static let secrect = "lMQb900L-mTeU-FVTCwyhjsfBwRCxwwbCitPob96cuU"
+    }
+}
