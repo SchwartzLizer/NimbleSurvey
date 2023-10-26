f15150ca49a9dc96074f69d56d3518d58e280d6a
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:00:54 +0700

    Add ButtonPresets.swift to project.pbxproj and implement applyThemeButton function in UIButton extension.
    - Add ButtonPresets.swift file to project.pbxproj
    - Implement applyThemeButton function in UIButton extension to customize button appearance

--- NimbleSurvey/NimbleSurvey/StyleSheet/ButtonPresets.swift
+++ NimbleSurvey/NimbleSurvey/StyleSheet/ButtonPresets.swift
@@ -5,4 +5,24 @@
 //  Created by Tanatip Denduangchai on 10/27/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    func applyThemeButton(
+        text: String,
+        font Font: UIFont,
+        color Color: UIColor,
+        round roundCorner: Double = 0,
+        backgroundColor BackgroundColor: UIColor = .clear,
+        borderColor BorderColor: UIColor = .clear)
+    {
+        self.setTitle(text.localized(), for: .normal)
+        self.titleLabel?.font = Font
+        self.setTitleColor(Color, for: .normal)
+        self.layer.cornerRadius = roundCorner
+        self.backgroundColor = BackgroundColor
+        self.layer.borderColor = BorderColor.cgColor
+        self.layer.borderWidth = 1
+    }
+
+}
