1ef89c16549635796b7202e97f37480f4e1c8be9
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:19:57 +0700

    Add TokenRefresher.swift to project
    This commit adds the TokenRefresher.swift file to the project. The TokenRefresher class is responsible for refreshing the token used for authentication. It includes a timer that triggers the token refresh at regular intervals. The refreshToken() method sends a network request to refresh the token and handles success and failure cases accordingly. The startTimer() and stopTimer() methods are used to control the timer's execution.

---
+++ NimbleSurvey/NimbleSurvey/Router/TokenRefresher.swift
@@ -0,0 +1,83 @@
+//
+//  TokenRefresher.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+class TokenRefresher {
+
+    // MARK: Lifecycle
+
+    // Private initializer to prevent creating different instances.
+    private init() { }
+
+    // MARK: Public
+
+    // Timer instance
+    public var refreshTokenTimer: Timer?
+
+    // Time interval for the timer (e.g., 60 minutes). Adjust as needed.
+    public let refreshInterval: TimeInterval = 7200 - 60
+
+    // Selector that refreshes the token
+    @objc
+    public func refreshToken() {
+        let router = Router.refreshToken(
+            grantType: "refresh_token",
+            refreshToken: UserDefault().getRefreshToken() ?? "",
+            clientID: Constants.ServiceKeys.key,
+            clientSecret: Constants.ServiceKeys.secrect)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success(let data):
+                self.onSuccess?()
+                guard let data = data.data?.attributes?.refreshToken else { return AlertUtility.showAlert(
+                    title: "Error",
+                    message: Constants.Keys.refresherTokenError.localized())
+                {
+                    AppUtility().loginScene()
+                }
+                }
+                UserDefault().saveRefreshToken(data: data)
+            case .failure:
+                self.onFailed?()
+                AlertUtility.showAlert(title: "Error", message: Constants.Keys.refresherTokenError.localized()) {
+                    AppUtility().loginScene()
+                }
+            }
+        }
+    }
+
+    // MARK: Internal
+
+    // Singleton instance
+    static let shared = TokenRefresher()
+
+    var onSuccess: (()->Void)?
+    var onFailed: (()->Void)?
+
+    // Method to start the timer
+    func startTimer() {
+        if self.refreshTokenTimer == nil {
+            self.refreshTokenTimer = Timer.scheduledTimer(
+                timeInterval: self.refreshInterval,
+                target: self,
+                selector: #selector(self.refreshToken),
+                userInfo: nil,
+                repeats: true)
+        }
+    }
+
+    // Method to stop the timer
+    func stopTimer() {
+        if self.refreshTokenTimer != nil {
+            self.refreshTokenTimer?.invalidate()
+            self.refreshTokenTimer = nil
+        }
+    }
+
+}
