b1bb35c6459df4e14594dc23dca841e87dcf1e85
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:23:02 +0700

    Add support for iOS 13 scene lifecycle
    This commit adds support for the iOS 13 scene lifecycle by implementing the `application(_:configurationForConnecting:options:)` and `application(_:didDiscardSceneSessions:)` methods in the `AppDelegate` class. It also adds the `@available(iOS 13.0, *)` attribute to these methods. Additionally, a new private method called `checkFontFamily()` is added to the `AppDelegate` class for checking font names after adding them. In the `SceneDelegate` class, the `@available(iOS 13.0, *)` attribute is added to indicate compatibility with iOS 13 or later versions.

--- NimbleSurvey/NimbleSurvey/AppDelegate.swift
+++ NimbleSurvey/NimbleSurvey/AppDelegate.swift
@@ -20,12 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -73,6 +75,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+    private func checkFontFamily() {
+        // For checo font name after add
+        for family in UIFont.familyNames {
+            print("\(family)")
+            for name in UIFont.fontNames(forFamilyName: family) {
+                print("   \(name)")
             }
         }
     }
