09f09b92b9f95f24ab8d815e782eede33701992b
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:48:55 +0700

    Add UIImageView extension for dimming effect
    This commit adds an extension to the UIImageView class that allows for adding a dimming effect to the image view. The extension includes a new method called "dim(with level: CGFloat)" which takes a level parameter to specify the opacity of the dimming effect.

--- NimbleSurvey/NimbleSurvey/Extension/UIImageView+Extension.swift
+++ NimbleSurvey/NimbleSurvey/Extension/UIImageView+Extension.swift
@@ -5,4 +5,13 @@
 //  Created by Tanatip Denduangchai on 10/27/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func dim(with level: CGFloat) {
+        let dimView = UIView(frame: bounds)
+        dimView.backgroundColor = UIColor.black.withAlphaComponent(level)
+        dimView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(dimView)
+    }
+}
