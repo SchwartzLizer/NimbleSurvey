e306ebde3c364faeaa23565c148c25c782ccdecc
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:30:19 +0700

    Refactor ForgotPasswordViewController and ForgotPasswordViewModel
    - Added a required initializer to ForgotPasswordViewController that takes in a viewModel parameter
    - Removed unnecessary code from viewDidLoad in ForgotPasswordViewController
    - Added public static properties nib and identifier to ForgotPasswordViewController
    - Created a new class called ForgotPasswordViewModel that inherits from ViewModel
    - Added an empty initializer to ForgotPasswordViewModel

--- NimbleSurvey/NimbleSurvey/Scene/ForgotPassword/ForgotPasswordViewController.swift
+++ NimbleSurvey/NimbleSurvey/Scene/ForgotPassword/ForgotPasswordViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class ForgotPasswordViewController: UIViewController {
 
+    // MARK: Lifecycle
+
+    required init(viewModel: ForgotPasswordViewModel) {
+        self.ViewModel = viewModel
+        super.init(nibName: ForgotPasswordViewController.identifier, bundle: nil)
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+    // MARK: Public
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
     }
-    */
+
+    public static var identifier: String {
+        return String(describing: self)
+    }
+
+    // MARK: Private
+
+    private var ViewModel: ForgotPasswordViewModel
 
 }
--- NimbleSurvey/NimbleSurvey/Scene/ForgotPassword/ForgotPasswordViewModel.swift
+++ NimbleSurvey/NimbleSurvey/Scene/ForgotPassword/ForgotPasswordViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+final class ForgotPasswordViewModel: ViewModel {
+
+    init() { }
+
+}
