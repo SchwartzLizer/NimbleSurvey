a736e0acd6211469d03a800820df8167017aecbc
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:32:17 +0700

    Refactor RightMenuView to use ViewModel pattern
    - Added RightMenuViewModel class to handle the logic and data for the RightMenuView
    - Initialized RightMenuViewModel with a model object in its constructor
    - Removed unused code from RightMenuViewController
    - Created a static property `nib` in RightMenuViewController to return the nib file associated with it
    - Created a static property `identifier` in RightMenuViewController to return its identifier string
    - Added a struct `RightMenuModel` to hold name and profile image information

--- NimbleSurvey/NimbleSurvey/Scene/Universal/RightMenuView/RightMenuViewController.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Universal/RightMenuView/RightMenuViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class RightMenuViewController: UIViewController {
 
+
+    // MARK: Lifecycle
+
+    required init(viewModel: RightMenuViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: RightMenuViewController.identifier, bundle: nil)
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    // MARK: Public
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
     }
-    */
+
+    public static var identifier: String {
+        return String(describing: self)
+    }
+
+    // MARK: Private
+
+    private var viewModel: RightMenuViewModel
 
 }
--- NimbleSurvey/NimbleSurvey/Scene/Universal/RightMenuView/RightMenuViewModel.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Universal/RightMenuView/RightMenuViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct RightMenuModel {
+
+    init(name: String, profileImage: String) {
+        self.name = name
+        self.profileImage = profileImage
+    }
+
+    let name:String
+    let profileImage:String
+}
