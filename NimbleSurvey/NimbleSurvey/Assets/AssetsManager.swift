87a65485c13998f354500e8f401e98984dcaa3aa
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:58:26 +0700

    Add AssetsManager.swift for managing assets in the project.
    - Added AssetsManager.swift file to the project.
    - Defined constants for Lottie animations, assets, and system images.

---
+++ NimbleSurvey/NimbleSurvey/Assets/AssetsManager.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsManager.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+extension Constants {
+    enum Lottie { }
+    enum Assest {
+        static let action = "Action"
+        static let notification = "Notification"
+    }
+    enum SystemImage { }
+}
