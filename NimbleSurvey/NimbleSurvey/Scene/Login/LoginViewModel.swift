fc9f100de19d2f8cd050da3bc2d53de9def8de99
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:22:29 +0700

    Add LoginViewModel for handling login functionality
    This commit adds a new file called LoginViewModel.swift which contains the implementation of the LoginViewModel class. The LoginViewModel class is responsible for handling the login functionality in the app.
    
    The LoginViewModel class conforms to the ViewModel protocol and implements the requestLogin method from the RequestService protocol. It also defines several closure properties for handling different scenarios such as successful login, failed login, no refresh token found, and no access token found.
    
    Inside the requestLogin method, it makes a network request using NetworkManager.shared.request to sign in with the provided email and password. Upon success, it saves the access token and refresh token to UserDefault using UserDefault().saveAccessToken(data:) and UserDefault().saveRefreshToken(data:) respectively. It then starts a timer using TokenRefresher.shared.startTimer().
    
    If there is an error during the network request, it checks if it's a server error or not. If it's a server error, it extracts the error details from the response and calls self.loginFailed? with that detail. Otherwise, it calls self.loginFailed? with the localized description of the error.
    
    These changes enable users to log in to their accounts successfully and handle any errors that may occur during login.

--- NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewModel.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewModel.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+// MARK: - LoginViewModel
+
+final class LoginViewModel: ViewModel {
+
+    // MARK: Lifecycle
+
+    init() { }
+
+    // MARK: Internal
+
+    var loginSuccess: (() -> Void)?
+    var loginFailed: ((_ message: String) -> Void)?
+    var noRefreshTokenFound: (() -> Void)?
+    var noAccessTokenFound: (() -> Void)?
+
+}
+
+// MARK: RequestService
+
+extension LoginViewModel: RequestService {
+    public func requestLogin(email: String, password: String) {
+        let router = Router.signIn(
+            grantType: "password",
+            email: email,
+            password: password,
+            clientID: Constants.ServiceKeys.key,
+            clientSecret: Constants.ServiceKeys.secrect)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success(let model):
+                guard let refreshToken = model.data?.attributes?.refreshToken else {
+                    self.noRefreshTokenFound?()
+                    return
+                }
+                guard let accessToken = model.data?.attributes?.accessToken else {
+                    self.noAccessTokenFound?()
+                    return
+                }
+                UserDefault().saveAccessToken(data: accessToken)
+                UserDefault().saveRefreshToken(data: refreshToken)
+                TokenRefresher.shared.startTimer()
+                self.loginSuccess?()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    guard let errors = errorResponse.errors.first?.detail else {
+                        self.loginFailed?(error.localizedDescription)
+                        return
+                    }
+                    self.loginFailed?(errors)
+                } else {
+                    self.loginFailed?(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
