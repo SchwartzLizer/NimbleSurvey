80ae71b29424a829a3821342fea2aefa45ac3fb9
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:30:05 +0700

    Add RouterEnumeration.swift to project.pbxproj and define ErrorType and Result enums
    - Add RouterEnumeration.swift file to project.pbxproj
    - Define ErrorType enum with cases for parseUrlFail, notFound, validationError, serverError, and defaultError
    - Define Result generic enum with cases for success(data:) and failure(error:)

---
+++ NimbleSurvey/NimbleSurvey/Router/RouterEnumeration.swift
@@ -0,0 +1,44 @@
+//
+//  RouterEnumeration.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+// MARK: - ErrorType
+
+enum ErrorType: LocalizedError {
+    case parseUrlFail
+    case notFound
+    case validationError
+    case serverError
+    case defaultError
+
+    // MARK: Internal
+
+    var errorDescription: String? {
+        switch self {
+        case .parseUrlFail:
+            return "Cannot initial URL object."
+        case .notFound:
+            return "Not Found"
+        case .validationError:
+            return "Validation Errors"
+        case .serverError:
+            return "Internal Server Error"
+        case .defaultError:
+            return "Something went wrong."
+        }
+    }
+}
+
+// MARK: - Result
+
+enum Result<T> {
+    case success(data: T)
+    case failure(error: Error)
+}
+
+
