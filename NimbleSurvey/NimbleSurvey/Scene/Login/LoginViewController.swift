4ab0cd23bb92dad1ea7d3b70e0df4b46239843ee
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:23:53 +0700

    Refactor LoginViewController.swift, Add lifecycle methods, internal properties, and private variables
    This commit adds lifecycle methods `viewDidDisappear`, internal properties `logoIconHeightConstraint`, `logoIconCenterYConstraint`, `textFieldBackgroundView`, `backgroundImageView`, `stackView`, `submitButtonView`, `forgotButtonView`, `emailTextField`, `passwordTextField`, and `logoIconImage`. It also adds private variables for logo position, theme, font, and viewModel.

--- NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewController.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 
+    // MARK: Internal
+
+    @IBOutlet weak var logoIconHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logoIconCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet var textFieldBackgroundView: [UIView]!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var submitButtonView: UIButton!
+    @IBOutlet weak var forgotButtonView: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logoIconImage: UIImageView!
+
+    // MARK: Private
+
+    private var logoPositionBelowNavBar: CGFloat = 0
+    private lazy var theme = StyleSheetManager.currentTheme()
+    private lazy var font = StyleSheetManager.currentFontTheme()
+    private var viewModel = LoginViewModel()
 }
 
