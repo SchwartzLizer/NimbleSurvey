71f918abd49f7404d9ac9d971acbdb22eebf71d3
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:57:52 +0700

    Add FontManager.swift to project.pbxproj and define font constants in Constants.swift
    - Add FontManager.swift to project.pbxproj
    - Define font constants in Constants.swift for regular and bold fonts

---
+++ NimbleSurvey/NimbleSurvey/Assets/FontManager.swift
@@ -0,0 +1,15 @@
+//
+//  FontManager.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+extension Constants {
+    enum Font {
+        static let regular = "NeuzeitSLTStd-Book"
+        static let bold = "NeuzeitSLTStd-BookHeavy"
+    }
+}
