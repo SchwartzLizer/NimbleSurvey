6f8cc0434b9f6db84fb30a6a60a982e0a185b06a
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:40:50 +0700

    Add theme customization to LoginViewController
    This commit adds the ability to apply a custom theme to the LoginViewController. The `applyTheme` function is implemented, which sets up various visual elements such as hiding and animating the stack view, resizing and animating the logo image, applying a blur effect to the background image, and customizing the appearance of text fields, submit button, and forgot button based on the selected theme.

--- NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewController.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Login/LoginViewController.swift
@@ -91,3 +91,101 @@ extension LoginViewController: Updated {
         }
     }
 }
+
+// MARK: ApplyTheme
+
+extension LoginViewController: ApplyTheme {
+
+    // MARK: Internal
+
+    internal func applyTheme() {
+        self.stackView.isHidden = true
+        self.stackView.alpha = 0
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.animateAndResizeImage()
+            self.blurBackgroundImage()
+
+            UIView.animate(withDuration: 1.0) {
+                self.stackView.isHidden = false
+                self.stackView.alpha = 1
+            }
+        }
+        self.applyThemeBackgroundTextField()
+        self.applyThemeSubmitButton()
+        self.applyThemeForgetButton()
+    }
+
+    // MARK: Private
+
+    private func animateAndResizeImage() {
+        self.logoPositionBelowNavBar = UIApplication.shared.statusBarFrame.height + 110
+        let logoCenterToEndPosition = self.logoPositionBelowNavBar + (self.logoIconImage.frame.height / 2.0)
+        let newCenterYConstant = logoCenterToEndPosition - (self.view.frame.height / 2.0)
+        let scale: CGFloat = 0.70
+        let scaledHeight = self.logoIconImage.frame.size.height * scale
+        UIView.animate(withDuration: 1, animations: {
+            self.logoIconCenterYConstraint.constant = newCenterYConstant
+            self.logoIconHeightConstraint.constant = scaledHeight
+            self.view.layoutIfNeeded()
+        })
+    }
+
+    private func blurBackgroundImage() {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurredEffectView = UIVisualEffectView(effect: blurEffect)
+        blurredEffectView.frame = self.backgroundImageView.bounds
+        blurredEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurredEffectView.alpha = 0
+        self.backgroundImageView.addSubview(blurredEffectView)
+        UIView.animate(withDuration: 1.0) {
+            blurredEffectView.alpha = 1
+        }
+    }
+
+    private func applyThemeBackgroundTextField() {
+        for view in self.textFieldBackgroundView {
+            view.applyThemeView(background: self.theme.textfieldBackgroundColor, radius: Constants.Radius.cornerRadiusCard)
+            let blurEffect = UIBlurEffect(style: .light)
+            let blurredEffectView = UIVisualEffectView(effect: blurEffect)
+            blurredEffectView.translatesAutoresizingMaskIntoConstraints = false
+            blurredEffectView.layer.cornerRadius = Constants.Radius.cornerRadiusCard
+            blurredEffectView.alpha = 1
+            blurredEffectView.clipsToBounds = true
+            blurredEffectView.frame = view.bounds
+            view.addSubview(blurredEffectView)
+        }
+        self.passwordTextField.borderStyle = .none
+        self.passwordTextField.isSecureTextEntry = true
+        self.passwordTextField.attributedPlaceholder = NSAttributedString(
+            string: Constants.Keys.passwordTF.localized(),
+            attributes: [NSAttributedString.Key.foregroundColor: self.theme.placeholderLabelColor])
+        self.passwordTextField.font = self.font.textLabelFontSize
+        self.passwordTextField.textColor = self.theme.textfieldLabelColor
+
+        self.emailTextField.borderStyle = .none
+        self.emailTextField.attributedPlaceholder = NSAttributedString(
+            string: Constants.Keys.emailTF.localized(),
+            attributes: [NSAttributedString.Key.foregroundColor: self.theme.placeholderLabelColor])
+        self.emailTextField.font = self.font.textLabelFontSize
+        self.emailTextField.textColor = self.theme.textfieldLabelColor
+    }
+
+    private func applyThemeSubmitButton() {
+        self.submitButtonView.applyThemeButton(
+            text: Constants.Keys.loginBTN,
+            font: self.font.buttonFontSize,
+            color: self.theme.buttonTextColor,
+            round: Constants.Radius.cornerRadiusCard,
+            backgroundColor: self.theme.buttonBackgroundColor,
+            borderColor: self.theme.buttonBackgroundColor)
+    }
+
+    private func applyThemeForgetButton() {
+        self.forgotButtonView.applyThemeButton(
+            text: Constants.Keys.forgotBTN,
+            font: self.font.forgotFontSize,
+            color: self.theme.forgotTextColor)
+    }
+}
+
