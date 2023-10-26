a8ceeda3562ddbc5eab61903709ffc73151e654d
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 01:00:27 +0700

    Add LabelPresets.swift to the project
    - Add LabelPresets.swift file to the project
    - Implement applyThemeLabel() function in UILabel extension
    - Apply font, color, and fitWidth properties to UILabel

---
+++ NimbleSurvey/NimbleSurvey/StyleSheet/ButtonPresets.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonPresets.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
--- NimbleSurvey/NimbleSurvey/StyleSheet/LabelPresets.swift
+++ NimbleSurvey/NimbleSurvey/StyleSheet/LabelPresets.swift
@@ -5,4 +5,16 @@
 //  Created by Tanatip Denduangchai on 10/27/23.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func applyThemeLabel(font: UIFont,color: UIColor,fitWidth: Bool = false) {
+        self.text = self.text
+        self.font = font
+        self.textColor = color
+        if fitWidth {
+            self.adjustsFontSizeToFitWidth = true
+            self.minimumScaleFactor = 0.5
+        }
+    }
+}
