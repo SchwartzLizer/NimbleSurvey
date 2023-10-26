25b943b8325a28cfa55a51d65e7425f4f695d568
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 00:38:02 +0700

    Add LogoutModel.swift to project.pbxproj
    - Added LogoutModel.swift to the project.pbxproj file
    - Created a new file called LogoutModel.swift in the Signin-Logout folder
    - Implemented the LogoutModel struct with Codable conformance

---
+++ NimbleSurvey/NimbleSurvey/Router/Model/Signin-Logout/LogoutModel.swift
@@ -0,0 +1,11 @@
+//
+//  LogoutModel.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+// MARK: - LogoutModel
+struct LogoutModel: Codable { }
