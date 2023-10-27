c4ee92e208cdd34087377b4fd658bf843a472a4c
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 10:29:33 +0700

    Add Welcome Survey Collection View Cell and ViewModel to HomeViewController.xib and project

---
+++ NimbleSurvey/NimbleSurvey/Scene/Home/Survey/WelcomeSurveyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeSurveyCollectionViewCell.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import UIKit
+
+class WelcomeSurveyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    var viewModel: WelcomeSurveyCollectionViewCell?
+
+}
---
+++ NimbleSurvey/NimbleSurvey/Scene/Home/Survey/WelcomeSurveyCollectionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeSurveyCollectionViewModel.swift
+//  NimbleSurvey
+//
+//  Created by Tanatip Denduangchai on 10/27/23.
+//
+
+import Foundation
+
+final class WelcomeSurveyCollectionViewModel {
+
+    init() { }
+}
