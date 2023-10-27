4a39d19a3d95c7f36711ae6f4431e91e321f3dc7
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 14:23:41 +0700

    Refactor language settings and remove unused test file
    - Refactored the language settings in the Snapfile to include only "en-US".
    - Removed the unused test file NimbleSurveyUITestsLaunchTests.swift from NimbleSurveyUITests directory.

--- NimbleSurvey/NimbleSurveyUITests/NimbleSurveyUITests.swift
+++ NimbleSurvey/NimbleSurveyUITests/NimbleSurveyUITests.swift
@@ -9,13 +9,26 @@ import XCTest
 
 final class NimbleSurveyUITests: XCTestCase {
 
-    override class func setUp() {
-        let app = XCUIApplication()
-        setupSnapshot(app)
-        app.launch()
-    }
-
     override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let app = XCUIApplication()
+        setupSnapshot(app) // Set up snapshot here if you haven't already
+        app.launch()
+
         continueAfterFailure = false
     }
+
+    func testTakeScreenshots() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+
+        // Navigate to the screen you want to take a screenshot of
+        // ... your UI navigation code goes here ...
+
+        // Take a screenshot with a specific name
+        snapshot("01FirstScreen") // The argument is used to name the screenshot
+
+        // Continue with other test operations and screenshots
+    }
+
 }
