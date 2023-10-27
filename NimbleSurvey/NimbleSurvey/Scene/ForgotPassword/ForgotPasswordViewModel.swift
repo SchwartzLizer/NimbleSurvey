56c948f26befe23ed8acddb3ea36163fb70d28cb
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 09:01:01 +0700

    Add ForgotPasswordViewModel and implement requestForgotPassword method
    This commit adds the `ForgotPasswordViewModel` class, which is responsible for handling the logic related to the forgot password feature. It also implements the `requestForgotPassword` method, which sends a network request to reset the user's password. The method handles both success and failure cases, calling appropriate callbacks based on the result.

--- NimbleSurvey/NimbleSurvey/Scene/ForgotPassword/ForgotPasswordViewModel.swift
+++ NimbleSurvey/NimbleSurvey/Scene/ForgotPassword/ForgotPasswordViewModel.swift
@@ -7,8 +7,44 @@
 
 import Foundation
 
+// MARK: - ForgotPasswordViewModel
+
 final class ForgotPasswordViewModel: ViewModel {
+
+    // MARK: Lifecycle
 
     init() { }
 
+    // MARK: Internal
+
+    var resetFailed: ((_ message: String) -> Void)?
+    var resetSuccess: (() -> Void)?
+}
+
+// MARK: - ForgotViewModel + RequestService
+
+extension ForgotPasswordViewModel: RequestService {
+    public func requestForgotPassword(email: String) {
+        let router = Router.forgotPassword(
+            email: email,
+            clientID: Constants.ServiceKeys.key,
+            clientSecret: Constants.ServiceKeys.secrect)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<ForgotPassword, NetworkError>) in
+            switch result {
+            case .success:
+                self.resetSuccess?()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    guard let errors = errorResponse.errors.first?.detail else {
+                        self.resetFailed?(error.localizedDescription)
+                        return
+                    }
+                    self.resetFailed?(errors)
+                } else {
+                    self.resetFailed?(error.localizedDescription)
+                }
+            }
+        }
+    }
 }
