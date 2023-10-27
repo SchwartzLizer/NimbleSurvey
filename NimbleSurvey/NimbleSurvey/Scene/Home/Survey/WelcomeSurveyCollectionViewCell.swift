f8a3ccef6f98a41cb86626a3dd77654515cd1f2d
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 10:37:26 +0700

    Refactor WelcomeSurveyCollectionViewCell.swift
    - Add MARK comments to organize code sections
    - Add public var viewModel for external use
    - Apply theme to titleLabel and subtitleLabel in applyThemeLabel() method

--- NimbleSurvey/NimbleSurvey/Scene/Home/Survey/WelcomeSurveyCollectionViewCell.swift
+++ NimbleSurvey/NimbleSurvey/Scene/Home/Survey/WelcomeSurveyCollectionViewCell.swift
@@ -7,20 +7,51 @@
 
 import UIKit
 
+// MARK: - WelcomeSurveyCollectionViewCell
+
 class WelcomeSurveyCollectionViewCell: UICollectionViewCell {
+
+    // MARK: Lifecycle
 
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+
+    // MARK: Public
+
+    public var viewModel: WelcomeSurveyCollectionViewCell?
+
+    // MARK: Internal
 
     @IBOutlet weak var startSuveyButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var backButtonView: UIButton!
 
-    var viewModel: WelcomeSurveyCollectionViewCell?
+    // MARK: Private
 
     private lazy var theme = StyleSheetManager.currentTheme()
     private lazy var font = StyleSheetManager.currentFontTheme()
 
 }
+
+// MARK: ApplyTheme
+
+extension WelcomeSurveyCollectionViewCell: ApplyTheme {
+
+    // MARK: Internal
+
+    internal func applyTheme() {
+        self.applyThemeLabel()
+    }
+
+    // MARK: Private
+
+    private func applyThemeLabel() {
+        self.titleLabel.applyThemeLabel(font: self.font.surveyTitleFontSize, color: self.theme.textLabelColor)
+        self.subtitleLabel.applyThemeLabel(font: self.font.surveySubtitleFontSize, color: self.theme.textLabelColor)
+    }
+
+
+}
+
