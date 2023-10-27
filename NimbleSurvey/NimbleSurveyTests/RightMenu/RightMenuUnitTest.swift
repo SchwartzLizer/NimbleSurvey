3432ecabfb3ae9f585021eca42f7174b19df7f66
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 10:07:10 +0700

    Add RightMenuViewModel and test methods
    - Added `RightMenuViewModel` class to handle right menu functionality.
    - Implemented `prepareRefreshToken` method to handle token preparation for login stage.
    - Added `testLogoutFailure` method to test logout failure scenario.
    - Added `testLoginSuccess` method to test login success scenario.

--- NimbleSurvey/NimbleSurveyTests/RightMenu/RightMenuUnitTest.swift
+++ NimbleSurvey/NimbleSurveyTests/RightMenu/RightMenuUnitTest.swift
@@ -9,26 +9,121 @@ import XCTest
 
 final class RightMenuUnitTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewModel: RightMenuViewModel!
+    var token: String?
+
+    override func setUp() {
+        super.setUp()
+        self.viewModel = RightMenuViewModel(model: RightMenuModel(name: "", profileImage: ""))
+        self.prepareRefreshToken()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        self.viewModel = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // MARK: Initalize
+    func prepareRefreshToken() {
+        let expectation = self.expectation(description: "Prepare login stage")
+        let grantType = "password"
+        let email = "dev@nimblehq.co"
+        let password = "12345678"
+        let clientID = Constants.ServiceKeys.key
+        let clientSecret = Constants.ServiceKeys.secrect
+
+        let router = Router.signIn(
+            grantType: grantType,
+            email: email,
+            password: password,
+            clientID: clientID,
+            clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success(let value):
+                Logger.print("Data \(value)")
+                guard let refreshToken = value.data?.attributes?.refreshToken
+                else { return XCTFail("Error, no token in response") }
+                self.token = refreshToken
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Login request failed with error: \(error)")
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // MARK: 403 - unauthorized_client
+    func testLogoutFailure() {
+        let expectation = self.expectation(description: "Logout should failed")
+        guard let token = self.token else {
+            XCTFail("Token not found")
+            return
+        }
+        let clientID = ""
+        let clientSecret = ""
+
+        let router = Router.logout(token: token, clientID: clientID, clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LogoutModel, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Expected failure for 'Logout should failed' but got success.")
+                expectation.fulfill()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    XCTAssertFalse(errorResponse.errors.isEmpty, "Expected non-empty errors list")
+                    let detail = errorResponse.errors.first?.detail ?? ""
+                    Logger.print(detail)
+                    XCTAssertEqual(
+                        detail,
+                        "You are not authorized to revoke this token")
+                } else {
+                    XCTFail("Expected server error with detail, got different error: \(error)")
+                }
+                expectation.fulfill()
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    // MARK: 200 - success
+    func testLoginSuccess() {
+        let expectation = self.expectation(description: "Logout should success")
+        guard let token = self.token else {
+            XCTFail("Token not found")
+            return
+        }
+        let clientID = Constants.ServiceKeys.key
+        let clientSecret = Constants.ServiceKeys.secrect
+
+        let router = Router.logout(token: token, clientID: clientID, clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LogoutModel, NetworkError>) in
+            switch result {
+            case .success(let value):
+                Logger.print("Data \(value)")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Logout failed with error: \(error)")
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
         }
     }
 
