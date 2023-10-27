92b3e7ecf31272799cae251312dd2e48223be79f
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 10:19:21 +0700

    Add unit tests for HomeViewModel
    This commit adds unit tests for the HomeViewModel class. The tests cover the following scenarios:
    
    - Initalize: Adds a test to prepare the refresh token.
    - Survey: Adds two tests to check for invalid and valid survey parameters.
    - User Profile: Adds a test to request user profile data.
    - Other Changes: Adds additional helper methods and tests for clearing data, processing dates, and updating scroll views.
    
    The purpose of these tests is to ensure that the HomeViewModel functions correctly and handles different scenarios as expected.

--- NimbleSurvey/NimbleSurveyTests/Home/HomeUnitTest.swift
+++ NimbleSurvey/NimbleSurveyTests/Home/HomeUnitTest.swift
@@ -9,27 +9,193 @@ import XCTest
 
 final class HomeUnitTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewModel: HomeViewModel!
+    var token: String?
+
+    override func setUp() {
+        super.setUp()
+        self.viewModel = HomeViewModel()
+        self.prepareRefreshToken()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        self.viewModel = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    // MARK: Initalize
+    func prepareRefreshToken() {
+        let expectation = self.expectation(description: "Prepare login stage")
+        let grantType = "password"
+        let email = "dev@nimblehq.co"
+        let password = "12345678"
+        let clientID = Constants.ServiceKeys.key
+        let clientSecret = Constants.ServiceKeys.secrect
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let router = Router.signIn(
+            grantType: grantType,
+            email: email,
+            password: password,
+            clientID: clientID,
+            clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success(let value):
+                Logger.print("Data \(value)")
+                guard let accessToken = value.data?.attributes?.accessToken
+                else { return XCTFail("Error, no token in response") }
+                self.token = accessToken
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Login request failed with error: \(error)")
+            }
         }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    // MARK: Survey
+    // MARK: 404 - invalid page number
+    func testSurveyInvalidPageNumber() {
+        let expectation = self.expectation(description: "Survey should invalid page number")
+        guard let token = self.token else {
+            XCTFail("Token not found")
+            return
+        }
+        let pageNumber = -100
+        let pageSize = 2
+
+        let router = Router.surveyList(page: pageNumber, perPage: pageSize, bearerToken: token)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<SurveyListModel, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Expected failure for 'Survey should invalid page number' but got success.")
+                expectation.fulfill()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    XCTAssertFalse(errorResponse.errors.isEmpty, "Expected non-empty errors list")
+                    let detail = errorResponse.errors.first?.detail ?? ""
+                    Logger.print(detail)
+                    XCTAssertEqual(
+                        detail,
+                        "You are not authorized to revoke this token")
+                } else {
+                    XCTFail("Expected server error with detail, got different error: \(error)")
+                }
+                expectation.fulfill()
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    // MARK: 200 - valid params
+    func testSurveyValidParams() {
+        let expectation = self.expectation(description: "Survey should success")
+        guard let token = self.token else {
+            XCTFail("Token not found")
+            return
+        }
+        let pageNumber = 100
+        let pageSize = 2
+
+        let router = Router.surveyList(page: pageNumber, perPage: pageSize, bearerToken: token)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<SurveyListModel, NetworkError>) in
+            switch result {
+            case .success(let value):
+                Logger.print("Data \(value)")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Logout failed with error: \(error)")
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    // MARK: User Profile
+    // MARK: 200 - success
+    func testRequestUser() {
+        let expectation = self.expectation(description: "Request user should success")
+        guard let token = self.token else {
+            XCTFail("Token not found")
+            return
+        }
+
+        let router = Router.userProfile(bearerToken: token)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<UserProfileModel, NetworkError>) in
+            switch result {
+            case .success(let value):
+                Logger.print("Data \(value)")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Logout failed with error: \(error)")
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    func testClearData() {
+        self.viewModel.clearData()
+        XCTAssertEqual(self.viewModel.datas.count, 0)
+        XCTAssertEqual(self.viewModel.lists.count, 0)
+    }
+
+    func testProcessDate() {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMMM d"
+        let expectedDate = dateFormatter.string(from: currentDate).uppercased()
+        let actualDate = self.viewModel.processDate()
+        XCTAssertEqual(
+            actualDate,
+            expectedDate,
+            "The processDate function should return the current date in 'EEEE, MMMM d' format.")
+    }
+
+    func testScrollViewUpdate() {
+        let expectation = self.expectation(description: "onScrollUpdated is called")
+        var callbackData: HomeDataModel?
+        let mockList = [
+            HomeDataModel(title: "Title1", subTitle: "Subtitle1"),
+            HomeDataModel(title: "Title2", subTitle: "Subtitle2"),
+            HomeDataModel(title: "Title3", subTitle: "Subtitle3"),
+        ]
+        self.viewModel.lists = mockList
+        self.viewModel.onScrollUpdated = { data in
+            callbackData = data
+            expectation.fulfill()
+        }
+        let testPage = 1
+        let result = self.viewModel.scrollViewUpdate(page: testPage)
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectations errored: \(error)")
+            }
+        }
+        XCTAssertEqual(callbackData, self.viewModel.lists[testPage], "Callback should be triggered with the correct data.")
+        XCTAssertEqual(result, self.viewModel.lists[testPage], "Should return the correct survey model for the page.")
     }
 
 }
