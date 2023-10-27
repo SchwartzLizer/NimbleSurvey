27f50c5cfda41fc870691fb057ee1ed97c4e0b72
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-27 08:55:55 +0700

    Refactor LoginUnitTest.swift for improved readability and maintainability
    - Refactored the setup and teardown methods to use the new syntax
    - Added comments to mark different test cases for better organization
    - Updated testSignInCorrectEmailOrPassword() to include a more descriptive expectation description
    - Updated testSignInInvalidGrant() to include a more descriptive expectation description
    - Updated testSignInInvaildClient() to include a more descriptive expectation description
    - Added comments to mark each test case with their respective HTTP status code

--- NimbleSurvey/NimbleSurveyTests/Login/LoginUnitTest.swift
+++ NimbleSurvey/NimbleSurveyTests/Login/LoginUnitTest.swift
@@ -7,29 +7,184 @@
 
 import XCTest
 
+import XCTest
+@testable import NimbleSurvey
+
 final class LoginUnitTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewModel: LoginViewModel!
+
+    override func setUp() {
+        super.setUp()
+        self.viewModel = LoginViewModel()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        self.viewModel = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    // MARK: 400 - incorrect password
+    func testSignInCorrectEmailOrPassword() {
+        let expectation = self.expectation(description: "Test Sign-In incorrect email or password")
+        let grantType = "password"
+        let email = "dev@nimblehq.co"
+        let password = "invalid"
+        let clientID = Constants.ServiceKeys.key
+        let clientScrect = Constants.ServiceKeys.secrect
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let router = Router.signIn(
+            grantType: grantType,
+            email: email,
+            password: password,
+            clientID: clientID,
+            clientSecret: clientScrect)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Expected failure for 'incorrect email or password' but got success.")
+                expectation.fulfill()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    XCTAssertFalse(errorResponse.errors.isEmpty, "Expected non-empty errors list")
+                    let detail = errorResponse.errors.first?.detail ?? ""
+                    Logger.print(detail)
+                    XCTAssertEqual(
+                        detail,
+                        "Your email or password is incorrect. Please try again.")
+                } else {
+                    XCTFail("Expected server error with detail, got different error: \(error)")
+                }
+                expectation.fulfill()
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
         }
     }
 
+    // MARK: 400 - invalid grant
+    func testSignInInvalidGrant() {
+        let expectation = self.expectation(description: "Test Sign-In invalid grant")
+        let grantType = "passwordd"
+        let email = "dev@nimblehq.co"
+        let password = "12345678"
+        let clientID = Constants.ServiceKeys.key
+        let clientSecret = Constants.ServiceKeys.secrect
+
+        let router = Router.signIn(
+            grantType: grantType,
+            email: email,
+            password: password,
+            clientID: clientID,
+            clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router)
+        { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Expected failure for 'invalid grant' but got success.")
+                expectation.fulfill()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    XCTAssertFalse(errorResponse.errors.isEmpty, "Expected non-empty errors list")
+                    let detail = errorResponse.errors.first?.detail ?? ""
+                    Logger.print(detail)
+                    XCTAssertEqual(
+                        detail,
+                        "The authorization grant type is not supported by the authorization server.")
+                } else {
+                    XCTFail("Expected server error with detail, got different error: \(error)")
+                }
+                expectation.fulfill()
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    // MARK: 403 - invalid client
+    func testSignInInvaildClient() {
+        let expectation = self.expectation(description: "Test Sign-In invalid client")
+        let grantType = "password"
+        let email = "dev@nimblehq.co"
+        let password = "invalid"
+        let clientID = "invalid_client_id"
+        let clientSecret = "invalid_client_secret"
+
+        let router = Router.signIn(
+            grantType: grantType,
+            email: email,
+            password: password,
+            clientID: clientID,
+            clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Expected failure for 'invalid grant' but got success.")
+                expectation.fulfill()
+            case .failure(let error):
+                if case .serverError(let errorResponse) = error {
+                    XCTAssertFalse(errorResponse.errors.isEmpty, "Expected non-empty errors list")
+                    let detail = errorResponse.errors.first?.detail ?? ""
+                    Logger.print(detail)
+                    XCTAssertEqual(
+                        detail,
+                        "Client authentication failed due to unknown client, no client authentication included, or unsupported authentication method.")
+                } else {
+                    XCTFail("Expected server error with detail, got different error: \(error)")
+                }
+                expectation.fulfill()
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
+
+    // MARK: 200 - success
+    func testLoginSuccess() {
+        let expectation = self.expectation(description: "Test Sign-In success")
+        let grantType = "password"
+        let email = "dev@nimblehq.co"
+        let password = "12345678"
+        let clientID = Constants.ServiceKeys.key
+        let clientSecret = Constants.ServiceKeys.secrect
+
+        let router = Router.signIn(
+            grantType: grantType,
+            email: email,
+            password: password,
+            clientID: clientID,
+            clientSecret: clientSecret)
+
+        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
+            switch result {
+            case .success(let value):
+                Logger.print("Data \(value)")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Login request failed with error: \(error)")
+            }
+        }
+
+        self.waitForExpectations(timeout: 10.0) { error in
+            if let error = error {
+                XCTFail("Expectation failed with error: \(error)")
+            }
+        }
+    }
 }
+
