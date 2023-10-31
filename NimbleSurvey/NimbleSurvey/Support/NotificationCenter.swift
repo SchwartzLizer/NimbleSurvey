b79ced1d22ebefa6f68350c3d5e71791b957aa2e
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-31 23:33:20 +0700

    Add new notification names for refresher token entering background and foreground
    - Add `refresherTokenAppDidEnterBackground` and `refresherTokenAppWillEnterForeground` as new notification names in the `NotificationCenter.swift` file.

--- NimbleSurvey/NimbleSurvey/Support/NotificationCenter.swift
+++ NimbleSurvey/NimbleSurvey/Support/NotificationCenter.swift
@@ -12,4 +12,6 @@ extension Notification.Name {
     static let backSurvey = Notification.Name("backSurvey")
     static let zoomInBackground = Notification.Name("zoomInBackground")
     static let zoomOutBackground = Notification.Name("zoomOutBackground")
+    static let refresherTokenAppDidEnterBackground = Notification.Name("refresherTokenAppDidEnterBackground")
+    static let refresherTokenAppWillEnterForeground = Notification.Name("refresherTokenAppWillEnterForeground")
 }
