e811ea681719814cf05b74de9c89e25ed9dfd15b
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-11-01 15:56:25 +0700

    feat: Add unit tests for HomeViewModel
    - Added a test case to verify the successful request of survey list
    - Added a test case to verify the successful request of user profile
    - Added a test case to handle failure error with local data
    - Added a test case to handle server error in failure error handling
    - Added a method to check if there is local survey list data available
    - Updated the tearDown method to remove survey list data from UserDefault

--- NimbleSurvey/NimbleSurveyTests/Home/HomeUnitTest.swift
+++ NimbleSurvey/NimbleSurveyTests/Home/HomeUnitTest.swift
@@ -10,192 +10,296 @@ import XCTest
 final class HomeUnitTest: XCTestCase {
 
     var viewModel: HomeViewModel!
-    var token: String?
+    var mockSession: MockURLSession!
 
     override func setUp() {
         super.setUp()
+        self.mockSession = MockURLSession()
+        NetworkManager.shared.setSession(self.mockSession)
         self.viewModel = HomeViewModel()
-        self.prepareRefreshToken()
     }
 
     override func tearDown() {
+        UserDefault().removeSurveyList()
+        self.mockSession = nil
         self.viewModel = nil
         super.tearDown()
     }
 
-    // MARK: Initalize
-    func prepareRefreshToken() {
-        let expectation = self.expectation(description: "Prepare login stage")
-        let grantType = "password"
-        let email = "dev@nimblehq.co"
-        let password = "12345678"
-        let clientID = Constants.ServiceKeys.key
-        let clientSecret = Constants.ServiceKeys.secrect
-
-        let router = Router.signIn(
-            grantType: grantType,
-            email: email,
-            password: password,
-            clientID: clientID,
-            clientSecret: clientSecret)
-
-        NetworkManager.shared.request(router: router) { (result: NetworkResult<LoginModel, NetworkError>) in
-            switch result {
-            case .success(let value):
-                Logger.print("Data \(value)")
-                guard let accessToken = value.data?.attributes?.accessToken
-                else { return XCTFail("Error, no token in response") }
-                self.token = accessToken
-                expectation.fulfill()
-            case .failure(let error):
-                XCTFail("Login request failed with error: \(error)")
-            }
-        }
-
-        self.waitForExpectations(timeout: 10.0) { error in
-            if let error = error {
-                XCTFail("Expectation failed with error: \(error)")
-            }
-        }
-    }
-
-    // MARK: Survey
-    // MARK: 404 - invalid page number
-    func testSurveyInvalidPageNumber() {
-        let expectation = self.expectation(description: "Survey should invalid page number")
-        guard let token = self.token else {
-            XCTFail("Token not found")
-            return
-        }
-        let pageNumber = -100
-        let pageSize = 2
-
-        let router = Router.surveyList(page: pageNumber, perPage: pageSize, bearerToken: token)
-
-        NetworkManager.shared.request(router: router) { (result: NetworkResult<SurveyListModel, NetworkError>) in
-            switch result {
-            case .success:
-                XCTFail("Expected failure for 'Survey should invalid page number' but got success.")
-                expectation.fulfill()
-            case .failure(let error):
-                if case .serverError(let errorResponse) = error {
-                    XCTAssertFalse(errorResponse.errors.isEmpty, "Expected non-empty errors list")
-                    let detail = errorResponse.errors.first?.detail ?? ""
-                    Logger.print(detail)
-                    XCTAssertEqual(
-                        detail,
-                        "You are not authorized to revoke this token")
-                } else {
-                    XCTFail("Expected server error with detail, got different error: \(error)")
+    func testHome_RequestSurveyList_Success() {
+        let jsonData = """
+            {
+                "data": [
+                    {
+                        "id": "d5de6a8f8f5f1cfe51bc",
+                        "type": "survey",
+                        "attributes": {
+                            "title": "Scarlett Bangkok",
+                            "description": "We'd love ot hear from you!",
+                            "thank_email_above_threshold": "",
+                            "thank_email_below_threshold": "",
+                            "is_active": true,
+                            "cover_image_url": "https://dhdbhh0jsld0o.cloudfront.net/m/1ea51560991bcb7d00d0_",
+                            "created_at": "2017-01-23T07:48:12.991Z",
+                            "active_at": "2015-10-08T07:04:00.000Z",
+                            "inactive_at": null,
+                            "survey_type": "Restaurant"
+                        },
+                        "relationships": {
+                            "questions": {
+                                "data": [
+                                    {
+                                        "id": "d3afbcf2b1d60af845dc",
+                                        "type": "question"
+                                    }
+                                ]
+                            }
+                        }
+                    }
+            ],
+                "meta": {
+                    "page": 1,
+                    "pages": 10,
+                    "page_size": 2,
+                    "records": 20
                 }
-                expectation.fulfill()
             }
-        }
-
-        self.waitForExpectations(timeout: 10.0) { error in
-            if let error = error {
-                XCTFail("Expectation failed with error: \(error)")
-            }
-        }
-    }
-
-    // MARK: 200 - valid params
-    func testSurveyValidParams() {
-        let expectation = self.expectation(description: "Survey should success")
-        guard let token = self.token else {
-            XCTFail("Token not found")
-            return
-        }
-        let pageNumber = 100
-        let pageSize = 2
-
-        let router = Router.surveyList(page: pageNumber, perPage: pageSize, bearerToken: token)
-
-        NetworkManager.shared.request(router: router) { (result: NetworkResult<SurveyListModel, NetworkError>) in
-            switch result {
-            case .success(let value):
-                Logger.print("Data \(value)")
-                expectation.fulfill()
-            case .failure(let error):
-                XCTFail("Logout failed with error: \(error)")
-            }
-        }
-
-        self.waitForExpectations(timeout: 10.0) { error in
-            if let error = error {
-                XCTFail("Expectation failed with error: \(error)")
-            }
-        }
-    }
-
-    // MARK: User Profile
-    // MARK: 200 - success
-    func testRequestUser() {
-        let expectation = self.expectation(description: "Request user should success")
-        guard let token = self.token else {
-            XCTFail("Token not found")
-            return
-        }
-
-        let router = Router.userProfile(bearerToken: token)
-
-        NetworkManager.shared.request(router: router) { (result: NetworkResult<UserProfileModel, NetworkError>) in
-            switch result {
-            case .success(let value):
-                Logger.print("Data \(value)")
-                expectation.fulfill()
-            case .failure(let error):
-                XCTFail("Logout failed with error: \(error)")
-            }
-        }
-
-        self.waitForExpectations(timeout: 10.0) { error in
-            if let error = error {
-                XCTFail("Expectation failed with error: \(error)")
-            }
-        }
-    }
-
-    func testClearData() {
-        self.viewModel.clearData()
-        XCTAssertEqual(self.viewModel.datas.count, 0)
-        XCTAssertEqual(self.viewModel.lists.count, 0)
-    }
-
-    func testProcessDate() {
-        let currentDate = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE, MMMM d"
-        let expectedDate = dateFormatter.string(from: currentDate).uppercased()
-        let actualDate = self.viewModel.processDate()
-        XCTAssertEqual(
-            actualDate,
-            expectedDate,
-            "The processDate function should return the current date in 'EEEE, MMMM d' format.")
-    }
-
-    func testScrollViewUpdate() {
-        let expectation = self.expectation(description: "onScrollUpdated is called")
-        var callbackData: HomeDataModel?
-        let mockList = [
-            HomeDataModel(title: "Title1", subTitle: "Subtitle1"),
-            HomeDataModel(title: "Title2", subTitle: "Subtitle2"),
-            HomeDataModel(title: "Title3", subTitle: "Subtitle3"),
-        ]
-        self.viewModel.lists = mockList
-        self.viewModel.onScrollUpdated = { data in
-            callbackData = data
+            """.data(using: .utf8)!
+        self.mockSession.data = jsonData
+        self.mockSession.urlResponse = HTTPURLResponse(
+            url: URL(string: "http://example.com")!,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil)
+        self.mockSession.error = nil
+        let expectation = XCTestExpectation(description: "Survey list request completed successfully")
+        self.viewModel.requestSurveyList(token: "testAccessToken") {
             expectation.fulfill()
         }
-        let testPage = 1
-        let result = self.viewModel.scrollViewUpdate(page: testPage)
-        waitForExpectations(timeout: 1) { error in
-            if let error = error {
-                XCTFail("waitForExpectations errored: \(error)")
-            }
-        }
-        XCTAssertEqual(callbackData, self.viewModel.lists[testPage], "Callback should be triggered with the correct data.")
-        XCTAssertEqual(result, self.viewModel.lists[testPage], "Should return the correct survey model for the page.")
+        wait(for: [expectation], timeout: 5.0)
+        XCTAssertEqual(self.viewModel.datas.count, 1)
     }
 
+    func testHome_RequestUserProfile_Success() {
+        let jsonData = """
+            {
+                "data": {
+                    "id": "1",
+                    "type": "user",
+                    "attributes": {
+                        "email": "dev@nimblehq.co",
+                        "name": "Team Nimble",
+                        "avatar_url": "https://secure.gravatar.com/avatar/6733d09432e89459dba795de8312ac2d"
+                    }
+                }
+            }
+            """.data(using: .utf8)!
+        self.mockSession.data = jsonData
+        self.mockSession.urlResponse = HTTPURLResponse(
+            url: URL(string: "http://example.com")!,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil)
+        self.mockSession.error = nil
+        let expectation = XCTestExpectation(description: "User profile request completed successfully")
+        self.viewModel.requestUserProfile(token: "testAccessToken") {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5.0)
+        XCTAssertEqual(self.viewModel.profileData.email, "dev@nimblehq.co")
+        XCTAssertEqual(self.viewModel.profileData.name, "Team Nimble")
+        XCTAssertEqual(self.viewModel.profileData.avatarURL, "https://secure.gravatar.com/avatar/6733d09432e89459dba795de8312ac2d")
+    }
+
+    func testHome_HandleFailureErrorWithLocalData() {
+        let mockSurveyListData: [SurveyListModelData] = [
+            SurveyListModelData(
+                id: "test",
+                type: "test",
+                attributes: SurveyListModelAttributes(
+                    title: "test",
+                    description: "test",
+                    thankEmailAboveThreshold: "test",
+                    thankEmailBelowThreshold: "test",
+                    isActive: false,
+                    coverImageURL: "test",
+                    createdAt: "test",
+                    activeAt: "test",
+                    inactiveAt: "test",
+                    surveyType: "test"),
+                relationships: SurveyListModelRelationships(questions: SurveyListModelQuestions(data: [QuestionsData(
+                    id: "test",
+                    type: "test")]))),
+        ]
+        UserDefault().saveSurveyList(data: mockSurveyListData)
+        let mockError = NetworkError.serverError(ErrorResponse(errors: []))
+        self.viewModel.handleFailureError(mockError)
+        XCTAssertEqual(self.viewModel.datas, mockSurveyListData)
+    }
+
+    func testHome_HandleFailureError_ServerError() {
+        let expectation = XCTestExpectation(description: "onFailed closure called")
+        self.viewModel.onFailed = { errorMessage in
+            XCTAssertEqual(errorMessage, "Server error message")
+            expectation.fulfill()
+        }
+        let mockServerError = NetworkError.serverError(ErrorResponse(errors: [ErrorResponse.Error(
+            source: nil,
+            detail: "Server error message",
+            code: "500")]))
+        self.viewModel.handleFailureError(mockServerError)
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testHome_CheckLocalDeviceData_SurveyListData() {
+        let mockSurveyListData: [SurveyListModelData] = [
+            SurveyListModelData(
+                id: "test",
+                type: "test",
+                attributes: SurveyListModelAttributes(
+                    title: "test",
+                    description: "test",
+                    thankEmailAboveThreshold: "test",
+                    thankEmailBelowThreshold: "test",
+                    isActive: false,
+                    coverImageURL: "test",
+                    createdAt: "test",
+                    activeAt: "test",
+                    inactiveAt: "test",
+                    surveyType: "test"),
+                relationships: SurveyListModelRelationships(questions: SurveyListModelQuestions(data: [QuestionsData(
+                    id: "test",
+                    type: "test")]))),
+        ]
+        UserDefault().saveSurveyList(data: mockSurveyListData)
+        let hasSurveyListData = self.viewModel.checkLocalDeviceData()
+        XCTAssertTrue(hasSurveyListData)
+    }
+
+    func testHome_CheckLocalDeviceDataWithoutSurveyListData() {
+        UserDefault().removeSurveyList()
+        let hasSurveyListData = self.viewModel.checkLocalDeviceData()
+        XCTAssertFalse(hasSurveyListData)
+    }
+
+    func testHome_ProcessData() {
+        let surveyListData: [SurveyListModelData] = [
+            SurveyListModelData(
+                id: "test",
+                type: "test",
+                attributes: SurveyListModelAttributes(
+                    title: "test",
+                    description: "test",
+                    thankEmailAboveThreshold: "test",
+                    thankEmailBelowThreshold: "test",
+                    isActive: false,
+                    coverImageURL: "test",
+                    createdAt: "test",
+                    activeAt: "test",
+                    inactiveAt: "test",
+                    surveyType: "test"),
+                relationships: SurveyListModelRelationships(questions: SurveyListModelQuestions(data: [QuestionsData(
+                    id: "test",
+                    type: "test")]))),
+            SurveyListModelData(
+                id: "test2",
+                type: "test2",
+                attributes: SurveyListModelAttributes(
+                    title: "test2",
+                    description: "test2",
+                    thankEmailAboveThreshold: "test2",
+                    thankEmailBelowThreshold: "test2",
+                    isActive: false,
+                    coverImageURL: "test2",
+                    createdAt: "test2",
+                    activeAt: "test2",
+                    inactiveAt: "test2",
+                    surveyType: "test2"),
+                relationships: SurveyListModelRelationships(questions: SurveyListModelQuestions(data: [QuestionsData(
+                    id: "test2",
+                    type: "test2")]))),
+        ]
+        let homeDataList = self.viewModel.processData(data: surveyListData)
+        XCTAssertEqual(homeDataList.count, surveyListData.count)
+        XCTAssertEqual(homeDataList[0].title, "test")
+        XCTAssertEqual(homeDataList[0].subTitle, "test")
+        XCTAssertEqual(homeDataList[1].title, "test2")
+        XCTAssertEqual(homeDataList[1].subTitle, "test2")
+    }
+
+    func testHome_ScrollViewUpdate() {
+        let mockHomeDataModel = HomeDataModel(title: "Test Title", subTitle: "Test Subtitle")
+        self.viewModel.lists = [mockHomeDataModel]
+        let expectation = XCTestExpectation(description: "onScrollUpdated closure called")
+        self.viewModel.onScrollUpdated = { updatedModel in
+            XCTAssertEqual(updatedModel, mockHomeDataModel)
+            expectation.fulfill()
+        }
+        let returnedHomeDataModel = self.viewModel.scrollViewUpdate(page: 0)
+        XCTAssertNotNil(returnedHomeDataModel)
+        if let unwrappedHomeDataModel = returnedHomeDataModel {
+            XCTAssertEqual(unwrappedHomeDataModel, mockHomeDataModel)
+        } else {
+            XCTFail("Returned HomeDataModel is nil")
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testHome_ProcessDate() {
+        let formattedDate = self.viewModel.processDate()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = Constants.DateFormat.eeeeMMMMd
+        let currentDate = Date()
+        let expectedFormattedDate = dateFormatter.string(from: currentDate).uppercased()
+        XCTAssertEqual(formattedDate, expectedFormattedDate)
+    }
+
+    func testHome_SaveSurveyData() {
+        let viewModel = HomeViewModel()
+        let surveyListData: [SurveyListModelData] = [
+            SurveyListModelData(
+                id: "test",
+                type: "test",
+                attributes: SurveyListModelAttributes(
+                    title: "test",
+                    description: "test",
+                    thankEmailAboveThreshold: "test",
+                    thankEmailBelowThreshold: "test",
+                    isActive: false,
+                    coverImageURL: "test",
+                    createdAt: "test",
+                    activeAt: "test",
+                    inactiveAt: "test",
+                    surveyType: "test"),
+                relationships: SurveyListModelRelationships(questions: SurveyListModelQuestions(data: [QuestionsData(
+                    id: "test",
+                    type: "test")]))),
+            SurveyListModelData(
+                id: "test2",
+                type: "test2",
+                attributes: SurveyListModelAttributes(
+                    title: "test2",
+                    description: "test2",
+                    thankEmailAboveThreshold: "test2",
+                    thankEmailBelowThreshold: "test2",
+                    isActive: false,
+                    coverImageURL: "test2",
+                    createdAt: "test2",
+                    activeAt: "test2",
+                    inactiveAt: "test2",
+                    surveyType: "test2"),
+                relationships: SurveyListModelRelationships(questions: SurveyListModelQuestions(data: [QuestionsData(
+                    id: "test2",
+                    type: "test2")]))),
+        ]
+        let mockSurveyListModel = SurveyListModel(
+            data: surveyListData,
+            meta: SurveyListModelMeta(page: 1, pages: 1, pageSize: 1, records: 1))
+        viewModel.save(data: mockSurveyListModel)
+        if let savedData = UserDefault().getSurveyList() {
+            XCTAssertNotNil(savedData)
+        } else {
+            XCTFail("Failed to retrieve saved survey data")
+        }
+    }
 }
